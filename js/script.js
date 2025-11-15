2a2051f247237f4531803305993a8080dd80fe90
José Vieira Lopes Neto <160689405+JV-Lopes@users.noreply.github.com>
2025-11-15 17:44:58 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-/* =============================typing animation============================= */
-var typed = new Typed(".typing", {
-    strings:["DEV Full-Stack"],
-    typeSpeed: 100,
-    backSpeed: 30,
-    loop: true
-})
